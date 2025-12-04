68064050900bd3e4d7fccd0478943e683929e610
Gbolahan28 <gbolahanfalana@gmail.com>
2025-12-04 10:40:42 +0100

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: "export", 
   images: {
     domains: ["detoplea.com", "blog.detopleamarketing.com"],
-  },
-  // Enable experimental features if needed
-  experimental: {
-    optimizePackageImports: ["lucide-react", "react-icons"],
-  },
-  // Proxy API requests if needed
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://localhost:3001/api/:path*",
-      },
-    ];
+    unoptimized: true, 
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
